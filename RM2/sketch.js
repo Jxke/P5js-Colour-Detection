7bfa4b1905e6ed5e48911eaa995e1e3ced8d52d6
burigetto <brigette.teo@gmail.com>
2023-01-19 11:43:26 +0800

    RM2 Commit (Brigette)

--- RM2/sketch.js
+++ RM2/sketch.js
@@ -1,7 +1,66 @@
-function setup() {
+let faceapi; //variable to store the api
+let detections = []; //variable to store anything that reutnrs from the api
 
+let video;
+let canvas;
+
+function setup() {
+    canvas = createCanvas(480,360);
+    canvas.id("canvas");
+
+    video = createCapture(VIDEO);
+    video.size(width,height);
+    video.id("video");
+
+    const faceOptions = {
+        withLandmarks: true,
+        withExpressions: true,
+        withDescriptors: true,
+        minConfidence: 0.5 //threshold for result
+    };
+
+    faceapi = ml5.faceApi(video, faceOptions, faceReady) //input, config, call back function
+
+}
+
+function faceReady(){
+    faceapi.detect(gotFaces);
+}
+
+function gotFaces(error,result){
+    if(error){
+        console.log(error);
+        return;
+    }
+    detections = result;
+
+    clear();
+    drawBoxes(detections);
+
+    console.log(detections);
+    faceapi.detect(gotFaces);
 }
 
 function draw() {
 
+    
+}
+
+function drawBoxes(detections){
+    if(detections.length >0){
+        for(f=0; f < detections.length; f++){
+            // let x = detections[f].alignedRect._box._x;
+            // let y = detections[f].alignedRect._box._y;
+            // let rectWidth = detections[f].alignedRect._box._width;
+            // let rectHeight = detections[f].alignedRect._box._height;
+
+            let {_x, _y, _width, _height} = detections[f].alignedRect.box;
+        
+
+            stroke(255,255,255);
+            strokeWeight(1);
+            noFill();
+            rect(_x,_y,_width,_height);
+        }
+    }
 }
