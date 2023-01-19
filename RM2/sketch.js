198e1621e2b935d35c5a4564f8ec7ba855346c9c
wikkiyah <vikkiliyiqi@gmail.com>
2023-01-19 10:50:44 +0800

    RM2

---
+++ RM2/sketch.js
@@ -0,0 +1,19 @@
+let faceapi; //variable to store the api
+let detections = []; //variable to store anything that returns from the api
+
+let video;
+let canvas;
+
+function setup() {
+    canvas = createCanvas(480,360);
+
+    video = createCapture(VIDEO);
+    video.size(width,height);
+
+    faceapi = ml5.faceApi(video,"config",)
+
+}
+
+function draw() {
+
+}
