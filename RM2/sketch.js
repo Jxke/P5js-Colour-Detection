34c8e0884d1cb48558ed795ae2f0af8ccd00e64c
Jxke <jxketan@gmail.com>
2023-01-19 10:50:14 +0800

    CP1

--- RM2/sketch.js
+++ RM2/sketch.js
@@ -1,4 +1,16 @@
+let faceapi; //variable to store the api
+let detections = []; //variable to store anything that returns from the api
+
+let video;
+let canvas;
+
 function setup() {
+    canvas = createCanvas(480, 360);
+
+    video = createCapture(VIDEO);
+    video.size(width,height);
+
+    faceapi = ml5.faceApi(video,_____,____)
 
 }
 
