5c992a74368769a170855cb13abc3400c49cae0d
Jxke <jxketan@gmail.com>
2023-01-19 11:42:28 +0800

    CP4 + FaceDetection

--- RM2/sketch.js
+++ RM2/sketch.js
@@ -6,8 +6,9 @@ let canvas;
 
 function setup() {
     canvas = createCanvas(480, 360);
-
+    canvas.id("canvas");
     video = createCapture(VIDEO);
+    video.id("video");
     video.size(width,height);
 
     const faceOptions = {
@@ -31,23 +32,30 @@ function gotFaces(error, result){
         return;
     }
     detections = result;
+
+    clear();
+    drawBoxes(detections);
+    
     console.log(detections);
+    faceapi.detect(gotFaces);
 }
 
 function draw() {
-    clear();
+
+}
+
+function drawBoxes(detections){
     if (detections.length > 0) {
         for (f=0; f < detections.length; f++){
-            let x = detections[f].alignedRect._box._x;
-            let y = detections[f].alignedRect._box._y;
-            let rectWidth = detections[f].alignedRect._box._width;
-            let rectHeight = detections[f].alignedRect._box._height;
-
+            // let x = detections[f].alignedRect._box._x;
+            // let y = detections[f].alignedRect._box._y;
+            // let rectWidth = detections[f].alignedRect._box._width;
+            // let rectHeight = detections[f].alignedRect._box._height;
+            let {_x, _y, _width, _height} = detections[f].alignedRect._box;
             stroke(255,255,255);
             strokeWeight(1);
             noFill();
-            rect(x,y,rectWidth,rectHeight);
+            rect(_x,_y,_width,_height);
         }
     }
-
 }
