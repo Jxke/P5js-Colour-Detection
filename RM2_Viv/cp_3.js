b7fa2ff260c4786e540eb676138ee118152e92a4
Viviantxh <viviantxh830@gmail.com>
2023-04-10 14:32:42 +0800

    RM2_Viv

---
+++ RM2_Viv/cp_3.js
@@ -0,0 +1,62 @@
+let faceapi; // variable to store faceapi
+let detections = []; // returns from faceapi
+
+let video;
+let canvas;
+
+function setup() {
+  canvas = createCanvas(480, 360);
+  canvas.id("canvas"); // calling canvas to be adopted
+  video = createCapture(VIDEO);// Create the video
+  video.id("video"); //*
+  video.size(width, height); 
+  const faceOptions = {
+    withLandmarks: true,
+    withExpressions: true,
+    withDescriptors: true,
+    minConfidence: 0.5 //threshold for result
+  };
+
+  faceapi = ml5.faceApi(video, faceOptions, faceReady);
+}
+
+function faceReady() {
+  faceapi.detect(gotFaces);// Start detecting faces
+}
+
+// Get faces
+function gotFaces(error, result) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  detections = result; //Now all the data in this detections
+  // console.log(detections);
+
+  clear();
+  drawBoxed(detections);
+
+  console.log(detections);
+  faceapi.detect(gotFaces);
+}
+
+function draw(){
+
+}
+
+function drawBoxes(detections){
+if (detections.length > 0) {//If at least 1 face is detected
+    for (f=0; f < detections.length; f++){
+    //   let x = detections[f].alignedRect._box._x;
+    //   let y = detections[f].alignedRect._box._y;
+    //   let rectWidth = detections[f].alignedRect._box._width;
+    //   let rectHeight = detections[f].alignedRect._box._height;
+    // command + ? = comment out
+    let{_x,_y,_width,_height} = detections[f].alignmentRect._box;
+      stroke(255, 255, 225);
+      strokeWeight(1);
+      noFill();
+      rect(_x,_y,_width,_height);
+    }
+  }
+}
---
+++ RM2_Viv/whiterect.js
@@ -0,0 +1,63 @@
+let faceapi; // variable to store faceapi
+let detections = []; // returns from faceapi
+
+let video;
+let canvas;
+let final;
+
+function setup() {
+    canvas = createCanvas(480, 360);
+    canvas.id("canvas");
+
+    video = createCapture(VIDEO);// Create the video
+    video.id("video");
+    video.size(width, height);
+    const faceOptions = {
+        withLandmarks: true,
+        withExpressions: true,
+        withDescriptors: true,
+        minConfidence: 0.5 //threshold for result
+    };
+
+    faceapi = ml5.faceApi(video, faceOptions, faceReady);
+}
+
+function faceReady() {
+    faceapi.detect(gotFaces);// Start detecting faces
+}
+
+// Get faces
+function gotFaces(error, result) {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    detections = result; //Now all the data in this detections
+    clear();
+    drawBoxes(detections);
+    console.log(detections);
+    faceapi.detect(gotFaces);
+
+}
+
+function draw() {
+
+}
+
+function drawBoxes(detections) {
+    if (detections.length > 0) {//If at least 1 face is detected
+        for (f = 0; f < detections.length; f++) {
+            let { _x, _y, _width, _height} = detections[f].alignedRect._box;
+            //console.log(_height);
+            stroke(255, 255, 225);
+            //strokeWeight(1);
+            //noFill();
+            //rect(_x, _y, _width, _height);
+
+            final = map(_height,200,90,255,0);// input, orign max, origin min, new max, new min
+
+            background (255,192,203,final);
+        }
+    }
+}
+
